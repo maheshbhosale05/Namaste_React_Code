8b12423ff5f9076d3920c185ecbd8aeae4e6b23a
Mahesh Bhosale <maheshbhosale0507@gmail.com>
2024-06-01 16:21:05 +0530

    09 Formik FOr for login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,45 @@
 import { useFormik } from "formik";
+import { useNavigate } from "react-router-dom";
 import React from "react";
 
 export const LoginForm = () => {
+  const navigate = useNavigate();
   const formik = useFormik({
     initialValues: { email: "", password: "" },
-    validate: () => ({}),
-    onSubmit: () => {},
+    validate: (value) => {
+      const errors = {};
+
+      if (!value.email) {
+        errors.email = "Email is Required.";
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value.email)
+      ) {
+        errors.email = "Invalid Email Address.";
+      }
+
+      if (!value.password) {
+        errors.password = "Password is Required.";
+      } else if (!/[A-Z]/.test(value.password)) {
+        errors.password = "At least one uppercase letter.";
+      } else if (!/[a-z]/.test(value.password)) {
+        errors.password = "At least one lower letter.";
+      } else if (!/[0-9]/.test(value.password)) {
+        errors.password = "At least one digit.";
+      } else if (!/[^A-Za-z0-9]/.test(value.password)) {
+        errors.password = "At least one special symbol.";
+      } else if (value.password.length < 8) {
+        errors.password = "Atleast 8 character required.";
+      }
+
+      return errors;
+    },
+    onSubmit: (value, { setSubmitting, resetForm }) => {
+      setTimeout(() => {
+        navigate("/");
+        setSubmitting(false);
+        resetForm();
+      }, 2000);
+    },
   });
   return (
     <div>
@@ -21,6 +55,7 @@ export const LoginForm = () => {
           onChange={formik.handleChange}
           onBlur={formik.handleBlur}
           value={formik.values.email}
+          disabled={formik.isSubmitting}
         />
         {formik.errors.email ? <div>{formik.errors.email}</div> : null}
 
@@ -39,15 +74,13 @@ export const LoginForm = () => {
           onChange={formik.handleChange}
           onBlur={formik.handleBlur}
           value={formik.values.password}
+          disabled={formik.isSubmitting}
         />
         {formik.errors.password ? <div>{formik.errors.password}</div> : null}
 
-        <br />
-        <br />
-        <br />
-        <br />
-
-        <button type="submit">Login</button>
+        <button type="submit" disabled={formik.isSubmitting}>
+          Login
+        </button>
       </form>
     </div>
   );
